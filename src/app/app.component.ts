5af0ca9330b2371fb555d49be9b4615a67f6429f
Luiz Eduardo <l.eduardo8316@gmail.com>
2020-02-22 21:55:04 -0300

    iniciando a tela de pesquisa dos lançamentos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'gerenciador-de-despesas-UI';
+
+  cars = [
+    {
+      marca: 'Honda',
+      modelo: 'Civic',
+      ano: '2008',
+      motor: '2.0'
+    },
+    {
+      marca: 'Wolksvagen',
+      modelo: 'Gol',
+      ano: '2006',
+      motor: '1.0'
+    },
+    {
+      marca: 'Wolksvagen',
+      modelo: 'Fox',
+      ano: '2010',
+      motor: '1.6'
+    },
+    {
+      marca: 'Ford',
+      modelo: 'Uno',
+      ano: '2015',
+      motor: '1.4'
+    }
+  ];
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { TableModule } from 'primeng/table';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    InputTextModule,
+    ButtonModule,
+    BrowserAnimationsModule,
+    TableModule
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
